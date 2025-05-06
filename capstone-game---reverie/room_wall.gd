3fdbda1a3194c10b6e7baf665a568d901089eb16
Erkhan73 <skhan163@fordham.edu>
2025-05-06 19:41:09 -0400

    Add files via upload

---
+++ capstone-game---reverie/room_wall.gd
@@ -0,0 +1,8 @@
+extends CSGBox3D
+
+# Called when the node enters the scene tree for the first time.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
